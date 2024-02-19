ddace9c37924899299ea52bd9020043dfecf9976
mi-skam <40042054+mi-skam@users.noreply.github.com>
2024-02-20 00:17:19 +0100

    works

--- src/js/scenes/Game.js
+++ src/js/scenes/Game.js
@@ -12,17 +12,33 @@ export default class Game extends Phaser.Scene {
     this._width = width;
     this._height = height;
 
-    this.load.image('bergwerk', 'assets/Berkwerk_Grundlage.jpg');
-    this.load.image('lichter', 'assets/Berkwerk_Lichter.png');
+    this.load.image('bergwerk', 'assets/Berkwerk_unbeleuchtet.jpg');
+    this.load.image('bergwerk_overlay', 'assets/Bergwerk_overlay.png');
+
+    for (let i = 1; i <= 6; i++) {
+      this.load.image(`lichter${i}`, `assets/Licht${i}.jpeg`);
+    }
   }
 
   create() {
     this.bergwerk = this.add.image(0, 0, 'bergwerk').setOrigin(0);
     this.bergwerk.setDisplaySize(this._width, this._height);
-    this.lichter = this.add.image(0, 0, 'lichter').setOrigin(0);
-    this.lichter.setDisplaySize(this._width, this._height);
 
-    this.lichter.visible = false;
+    for (let i = 1; i <= 6; i++) {
+      const lichter = this.add.image(0, 0, `lichter${i}`).setOrigin(0);
+      lichter.setDisplaySize(this._width, this._height);
+      lichter.setBlendMode(Phaser.BlendModes.SCREEN);
+      lichter.visible = false;
+      lichter.name = `lichter${i}`;
+
+      this[`lichter${i}`] = lichter;
+    }
+
+    this.bergwerkOverlay = this.add
+      .image(0, 0, 'bergwerk_overlay')
+      .setOrigin(0);
+    this.bergwerkOverlay.setDisplaySize(this._width, this._height);
+    this.bergwerkOverlay.setBlendMode(Phaser.BlendModes.OVERLAY);
 
     HandTracking.initialize({
       hands: 2,
@@ -33,7 +49,6 @@ export default class Game extends Phaser.Scene {
       this._handTracking = tracker;
 
       // gui
-      // !!! - (debug) - replaced it with my local variant
       webcamGui();
     });
   }
@@ -44,17 +59,44 @@ export default class Game extends Phaser.Scene {
     const results = this._handTracking.getHands(this._width, this._height);
 
     if (results.detected) {
+      this.setAllHandsInvisible();
+
       if (results.handLeft?.gesture === 'Closed_Fist') {
-        this.lichter.visible = true;
-      } else {
-        this.lichter.visible = false;
+        const closest = this.selectClosestLichtGroup(results.handLeft);
+        closest.visible = true;
       }
 
       if (results.handRight?.gesture === 'Closed_Fist') {
-        this.lichter.visible = true;
-      } else {
-        this.lichter.visible = false;
+        const closest = this.selectClosestLichtGroup(results.handRight);
+        closest.visible = true;
       }
     }
+  }
+
+  setAllHandsInvisible() {
+    for (let i = 1; i <= 6; i++) {
+      this[`lichter${i}`].visible = false;
+    }
+  }
+
+  selectClosestLichtGroup(hand) {
+    if (!hand) throw new Error(`No hand detected.`);
+
+    let closestLicht = null;
+    let _closestDist = Number.POSITIVE_INFINITY;
+
+    for (let i = 1; i <= 2; i++) {
+      const y = this._height * i * (1 / 2);
+      for (let j = 1; j <= 3; j++) {
+        const x = this._width * j * (1 / 3);
+        const _dist = Phaser.Math.Distance.Between(hand.x, hand.y, x, y);
+        if (_dist < _closestDist) {
+          closestLicht = this[`lichter${i * j}`];
+          _closestDist = _dist;
+        }
+      }
+    }
+    console.log(closestLicht);
+    return closestLicht;
   }
 }
